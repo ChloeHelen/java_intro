8bd6fc7056e53df6554643f26e8cca3c9eb22834
ChloeHelen <cmingay@fastmail.fm>
2018-10-18 14:20:52 +0100

    Arrays

--- chapter2-DataTypes/exercises/MyArrays.java
+++ chapter2-DataTypes/exercises/MyArrays.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 // can you remember how to set up your main function?
 public class MyArrays {
   public static void main (String[] args) {
@@ -7,7 +8,13 @@ public class MyArrays {
   String[] arrayOfWords = new String[] {"Hello", "there", "Chloe"};
   System.out.println(Arrays.toString(arrayOfWords));
 // declare an array of integers and print it
-
+  int[] arrayOfInts = new int[] {1, 2, 3};
+  System.out.println(Arrays.toString(arrayOfInts));
 // declare an ArrayList of integers, add numbers to it, and then print it
+  ArrayList al = new ArrayList();
+  al.add("1");
+  al.add("2");
+  al.add(5);
+  System.out.println(al);
   }
 }
