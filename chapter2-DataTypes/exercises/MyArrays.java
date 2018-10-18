0f21d934ab4625f8e35ff0a079f51de2062fe030
ChloeHelen <cmingay@fastmail.fm>
2018-10-18 12:33:19 +0100

    Array of strings

---
+++ chapter2-DataTypes/exercises/MyArrays.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+// can you remember how to set up your main function?
+public class MyArrays {
+  public static void main (String[] args) {
+
+// declare an array of Strings and print it
+  String[] arrayOfWords = new String[] {"Hello", "there", "Chloe"};
+  System.out.println(Arrays.toString(arrayOfWords));
+// declare an array of integers and print it
+
+// declare an ArrayList of integers, add numbers to it, and then print it
+  }
+}
--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,19 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String first_name = "Chloe";
     // declare the second name
-
+    String last_name = "Helen";
     // create your message
-
+    // StringBuilder
+    StringBuilder message1 = new StringBuilder()
+    .append(first_name).append(" ").append(last_name);
+    // stringFormatConcat
+    String message2 = String.format("%s %s", first_name, last_name);
     // print your message!
-
+    System.out.println(message1);
+    System.out.println(message2);
   }
 }
+
+// String stringFormatConcat = String.format("%s %s! Welcome to the %s!", greeting, person, location);
